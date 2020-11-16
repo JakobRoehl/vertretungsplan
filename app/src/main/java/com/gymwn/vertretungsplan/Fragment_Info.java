c7cec6a2b6e599e0ea9e7ae71b95c929cb4df8fd
Mojo741m <74373773+Mojo741m@users.noreply.github.com>
2020-11-16 21:50:23 +0100

    fragment_info.xml bearbeitet

--- app/src/main/java/com/gymwn/vertretungsplan/Fragment_Info.java
+++ app/src/main/java/com/gymwn/vertretungsplan/Fragment_Info.java
@@ -1,9 +1,13 @@
 package com.gymwn.vertretungsplan;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +18,20 @@ public class Fragment_Info extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container,false);
+
+        TextView version_tv = view.findViewById(R.id.version_tv);
+        TextView link_tv = view.findViewById(R.id.link_tv);
+
+        try {
+            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            String version = pInfo.versionName;
+            version_tv.setText("Version: " + version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        link_tv.setText(Html.fromHtml("<a href=\"https://www.gym-nw.de/\">gym-nw.de</a>"));
+
         return view;
     }
 }
