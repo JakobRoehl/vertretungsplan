44f8c653bfbf65a9cd9b0d3df68e878e7fbb02e1
Mojo741m <74373773+Mojo741m@users.noreply.github.com>
2020-11-18 21:14:23 +0100

    Notifications 1.2

--- app/src/main/java/com/gymwn/vertretungsplan/Fragment_notification.java
+++ app/src/main/java/com/gymwn/vertretungsplan/Fragment_notification.java
@@ -13,11 +13,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class Fragment_notification extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notification, container,false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         return view;
     }
